dc0e8f70faeed1aca0df31a7e5a80225931c82a8
Denise Case <dcase@nwmissouri.edu>
2019-07-28 18:39:54 -0500

    F19

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.use(morgan('combined'))
 app.use('/todo', require('./routes/todo'))
 app.use('/auth', require('./routes/auth'))
 app.use('/user', require('./routes/user'))
-
-app.use('/', (req,res)=> {res.send('Try /todo or /auth or /user')})
+app.use('/', (req,res)=> {res.send('Try /todo')})
 
 // Connect to data store
 mongoose
